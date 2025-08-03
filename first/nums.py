eab05546a6d1c683aa66158706d92063664d9043
vpokora70 <vpokora@gmail.com>
2025-08-03 15:29:30 +0300

    Third commit added module strings, and func "mul"

--- first/nums.py
+++ first/nums.py
@@ -2,12 +2,17 @@
 def plus(a, b):
     return a + b
 
-text = "new text"
 
 def minus(a, b):
     return a - b
+
+def mul(a, b):
+    return a * b
+
+
 
 
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(5, 2))
+    print(mul(6, 6))
