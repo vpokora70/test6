7e829f4b9235f53c2d9d2dc2251b87e4f91b7435
vpokora70 <vpokora@gmail.com>
2025-08-03 15:18:52 +0300

    Second commit, added func "minus"

--- first/nums.py
+++ first/nums.py
@@ -2,6 +2,12 @@
 def plus(a, b):
     return a + b
 
+text = "new text"
+
+def minus(a, b):
+    return a - b
+
 
 if __name__ == '__main__':
-    print(plus(2, 2)
+    print(plus(2, 2))
+    print(minus(5, 2))
